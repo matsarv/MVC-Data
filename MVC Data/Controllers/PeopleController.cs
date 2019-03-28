ca2187e0dcbbd33a10bc05b0e39e9a8ee98e8843
matsarv <mats.arvidsson@outlook.com>
2019-03-28 14:01:47 +0100

    Add project files.

---
+++ MVC Data/Controllers/PeopleController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Data.Models;
+
+namespace MVC_Data.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        IPersonService _personService;
+
+        public PeopleController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+        
+        // GET
+        [HttpGet]
+        public IActionResult Index(string searchString)
+        {
+            PersonView pv = new PersonView();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                pv.persons = _personService.FilterPersonCity(searchString);
+            }
+            else
+            {
+                pv.persons = _personService.AllPersons();
+            }
+            return View(pv);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Person person = _personService.FindPerson((int)id);
+
+            if (person == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _personService.DeletePerson((int)id);
+
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Create(string name, int phone, string city)
+        {
+            if (name == null||  city == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Person person = _personService.CreatePerson( name,  phone,  city);
+
+            return RedirectToAction("Index");
+
+        }
+
+    }
+
+}
+
---
+++ MVC Data/Models/MockPersonService.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Data.Models
+{
+    public class MockPersonService : IPersonService
+    {
+        PersonView pv = new PersonView();
+
+        private int idCount = 1;
+
+        public MockPersonService()
+        {
+            pv.persons.Add(new Person() { Id = 0, Name = "Nisse Person", Phone = 0731234567, City = "Växjö" });
+         }
+
+        public List<Person> AllPersons()
+        {
+            return pv.persons;
+        }
+        
+        public Person CreatePerson(string name, int phone, string city)
+        {
+            Person person = new Person() { Id = idCount, Name = name, Phone = phone, City = city };
+            idCount++;
+            pv.persons.Add(person);
+
+            return person;
+        }
+
+        public bool DeletePerson(int id)
+        {
+            bool wasRemoved = false;
+
+            foreach (Person item in pv.persons)
+            {
+                if (item.Id == id)
+                {
+                    pv.persons.Remove(item);
+
+                    wasRemoved = true;
+                    break;
+                }
+            }
+
+            return wasRemoved;
+        }
+
+        public Person FindPerson(int id)
+        {
+            foreach (Person item in pv.persons)
+            {
+                if (item.Id == id)
+                {
+                    return item;
+                }
+            }
+
+            return null;
+        }
+
+        // Not in use yet
+        public bool UpdatePerson(Person person)
+        {
+            bool wasUpdated = false;
+
+            foreach (Person orginal in pv.persons)
+            {
+                if (orginal.Id == person.Id)
+                {
+                    orginal.Name = person.Name;
+                    orginal.Phone = person.Phone;
+                    orginal.City = person.City;
+
+                    wasUpdated = true;
+                    break;
+                }
+            }
+
+            return wasUpdated;
+        }
+
+        public List<Person> FilterPersonCity(string searchString)
+        {
+            var people = pv.persons.Where(s => s.Name.Contains(searchString) || s.City.Contains(searchString)).ToList();
+
+            return (people);
+        }
+
+    }
+}
